78e992b102902507c44e70d43be1f36087811857
Keyukemi <keyukemi.ku@gmail.com>
2025-08-15 20:52:40 +0100

    Remove OriginalLanding.jsx from main deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import OriginalLanding from './OriginalLanding.jsx'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/originalLanding',
-    element: <OriginalLanding />,
   },
 ])
 
